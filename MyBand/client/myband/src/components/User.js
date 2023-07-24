80fa4be491eacb6b9b816aa990bc2e30de36f99f
John Brady <johntbrady01@gmail.com>
2023-07-23 22:26:09 -0400

    userCss

--- MyBand/client/myband/src/components/User.js
+++ MyBand/client/myband/src/components/User.js
@@ -1,14 +1,40 @@
 import { NavLink as RRNavLink } from "react-router-dom";
 import { useNavigate } from "react-router-dom"
+import { CardBody } from "reactstrap";
+import { CardTitle } from "reactstrap";
+import { Card } from "reactstrap";
 
 export const User = ({ user }) => {
 
     const navigate = useNavigate()
 
     return (
-        <div>
-            <h4>{user.name}</h4>
-            <img src={user.profilePic} alt="image" />
-        </div>
+        // <div>
+        //     <h4>{user.name}</h4>
+        //     <img src={user.profilePic} alt="image" />
+        // </div>
+        <Card
+            style={{
+                width: '12rem',
+                color: "success",
+                margin: '2rem'
+
+            }}
+        >
+            <img
+                alt="Sample"
+                src={user.profilePic}
+                style={{
+                    width: '12rem',
+                    height: '12rem',
+                    objectFit: 'cover'
+                }}
+            />
+            <CardBody>
+                <CardTitle tag="h5">
+                    {user.name}
+                </CardTitle>
+            </CardBody>
+        </Card>
     )
 }
