7a042392585d8bfd137fe31678f9b1b68e1fba0a
John Brady <johntbrady01@gmail.com>
2023-07-27 17:13:45 -0400

    editMembers

---
+++ MyBand/client/myband/src/components/EditMembers.js
@@ -0,0 +1,111 @@
+import { useEffect, useState } from "react"
+import { useNavigate } from "react-router-dom"
+import { CardBody } from "reactstrap";
+import { CardTitle } from "reactstrap";
+import { Card } from "reactstrap";
+
+export const EditMembers = ({ member, userProfile }) => {
+    const navigate = useNavigate()
+
+    const makeLeaderButton = () => {
+        {
+            return <button onClick={() => {
+                fetch(`/api/BandUserRequest`, {
+                    method: "PUT",
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                    body: JSON.stringify({
+                        id: member.id,
+                        userId: member.userId,
+                        roleId: member.roleId,
+                        bandId: member.bandId,
+                        isLeader: true,
+                        isAccepted: member.isAccepted,
+                        note: member.note,
+                        sentByBand: member.sentByBand
+
+                    })
+
+                })
+                    .then(() => {
+                        navigate(`/Band/${member.bandId}`)
+                    })
+
+            }} className="request__accept">Make Leader</button>
+        }
+
+    }
+
+    const deleteButton = () => {
+        return <button onClick={() => {
+
+            fetch(`/api/BandUserRequest/${member.id}`, {
+                method: "DELETE"
+            })
+                .then(() => {
+                    navigate(`/Band/${member.bandId}`)
+                })
+
+        }} className="request__delete">Remove From Band</button>
+    }
+
+
+
+
+
+    return (
+        <>
+            <div className="requestDiv">
+                {
+                    (member?.userId == userProfile.id)
+                        ? <>
+                        </>
+                        : <>
+                            <div>
+                                <Card
+                                    style={{
+                                        width: '12rem',
+                                        color: "success",
+                                        margin: '2rem'
+
+                                    }}
+                                >
+                                    <img
+                                        alt="Sample"
+                                        src={member?.user?.profilePic}
+                                        style={{
+                                            width: '12rem',
+                                            height: '12rem',
+                                            objectFit: 'cover'
+                                        }}
+                                    />
+                                    <CardBody>
+                                        <CardTitle tag="h5"
+                                            style={{
+                                                display: 'flex',
+                                                justifyContent: "space-between"
+                                            }}
+                                        >
+                                            {member?.user?.name}
+                                        </CardTitle>
+                                    </CardBody>
+                                </Card>
+                                {member?.user.role}
+                            </div>
+                            {
+                                (member?.isLeader)
+                                    ? <>
+                                    </>
+                                    : <>
+                                        <div className="button">{makeLeaderButton()}</div>
+                                    </>
+
+                            }
+                            <div className="button">{deleteButton()}</div>
+                        </>
+                }
+            </div>
+        </>
+    )
+}
