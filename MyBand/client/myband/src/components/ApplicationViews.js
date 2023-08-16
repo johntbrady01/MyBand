d1949b62134fc32fa0a3ed995a30152f267c460a
John Brady <johntbrady01@gmail.com>
2023-08-16 19:09:28 -0400

    loginCss

--- MyBand/client/myband/src/components/ApplicationViews.js
+++ MyBand/client/myband/src/components/ApplicationViews.js
@@ -22,13 +22,14 @@ export default function ApplicationViews({ isLoggedIn, userProfile }) {
     return (
         <main>
             <Routes>
+                <Route path="login" element={<Login />} />
+                <Route path="register" element={<Register />} />
                 <Route path="/">
                     <Route
                         index
                         element={isLoggedIn ? <UserList userProfile={userProfile} /> : <Navigate to="/login" />}
                     />
                     <Route path="bands" element={<BandList />} />
-                    <Route path="login" element={<Login />} />
                     <Route path="User/:userId" element={<UserDetails userProfile={userProfile} />} />
                     <Route path="/bands/create" element={<AddBand userProfile={userProfile} />} />
                     <Route path="/bandrequest/:bandId" element={<BandUserRequest userProfile={userProfile} />} />
@@ -39,7 +40,6 @@ export default function ApplicationViews({ isLoggedIn, userProfile }) {
                     <Route path="/deleteband/:bandId" element={<DeleteBand userProfile={userProfile} />} />
                     <Route path="/bandrequests/:bandId" element={<SeeBandRequests userProfile={userProfile} />} />
                     <Route path="Band/:bandId" element={<BandDetails userProfile={userProfile} />} />
-                    <Route path="register" element={<Register />} />
                     <Route path="*" element={<p>Whoops, nothing here...</p>} />
                 </Route>
             </Routes>
--- MyBand/client/myband/src/components/Login.js
+++ MyBand/client/myband/src/components/Login.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
 import { useNavigate, Link } from "react-router-dom";
 import { login } from "../modules/authManager";
+import logo from "../photos/logo.png"
 
 
 export default function Login() {
@@ -17,33 +18,50 @@ export default function Login() {
             .catch(() => alert("Invalid email or password"));
     };
 
-    return (
-        <Form onSubmit={loginSubmit}>
-            <fieldset>
-                <FormGroup>
-                    <Label for="email">Email</Label>
-                    <Input
-                        id="email"
-                        type="text"
-                        autoFocus
-                        onChange={(e) => setEmail(e.target.value)}
-                    />
-                </FormGroup>
-                <FormGroup>
-                    <Label for="password">Password</Label>
-                    <Input
-                        id="password"
-                        type="password"
-                        onChange={(e) => setPassword(e.target.value)}
-                    />
-                </FormGroup>
-                <FormGroup>
-                    <Button>Login</Button>
-                </FormGroup>
-                <em>
-                    Not registered? <Link to="/register">Register</Link>
-                </em>
-            </fieldset>
-        </Form>
-    );
+    return <>
+        <div className="LoginPage">
+            <div className="Login">
+                <Form onSubmit={loginSubmit}>
+                    <fieldset>
+                        <h2>Login</h2>
+                        <hr />
+                        <FormGroup>
+                            <Label for="email">Email</Label>
+                            <Input
+                                id="email"
+                                type="text"
+                                autoFocus
+                                onChange={(e) => setEmail(e.target.value)}
+                            />
+                        </FormGroup>
+                        <FormGroup>
+                            <Label for="password">Password</Label>
+                            <Input
+                                id="password"
+                                type="password"
+                                onChange={(e) => setPassword(e.target.value)}
+                            />
+                        </FormGroup>
+                        <FormGroup>
+                            <Button style={{ backgroundColor: 'rgb(253,216,19)', color: 'black', width: '100%' }}>Login</Button>
+                        </FormGroup>
+                        <em>
+                            Not registered? <Link to="/register">Register</Link>
+                        </em>
+                    </fieldset>
+                </Form>
+            </div>
+            <img src={logo}
+                style={{
+                    width: '17rem',
+                    height: '20rem',
+                    marginRight: '17rem',
+                    marginTop: '5rem'
+
+
+
+
+                }} />
+        </div>
+    </>
 }
